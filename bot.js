d2e99e700dc3938dcb777bfcc393a0b16e0bb410
William <wzhouwzhou@gmail.com>
2017-05-29 01:37:38 -0400

    Quality of life improvements

--- bot.js
+++ bot.js
@@ -7,17 +7,19 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
+  let channel = msg.channel;
+
   if (msg.content === prefix + 'Sinbadx')
-    msg.channel.sendMessage('The one, the only!');
+    channel.send('The one, the only!');
   else
   if (msg.content === prefix + 'Xena')
-    msg.channel.sendMessage('Do not touch my master!');
+    channel.send('Do not touch my master!');
   else
   if (msg.content === prefix + 'Pixel')
-    msg.channel.sendMessage('Pixelate that. - Pixel, 2k17');
+    channel.send('Pixelate that. - Pixel, 2k17');
   else
   if (msg.content === prefix + 'ArX')
-    msg.channel.sendMessage('Feel the power of the almighty ban hammer!');
+    channel.send('Feel the power of the almighty ban hammer!');
 });
 
 client.login(process.env.TOKEN);
