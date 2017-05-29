791942a61902b5b0062daea35f6cbe8aaf68ec71
William <wzhouwzhou@gmail.com>
2017-05-29 01:03:57 -0400

    First commit
    Yey DonutBot’s first day

---
+++ bot.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = ">"
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.username}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === prefix + 'Sinbadx') {
+    msg.channel.sendMessage('The one, the only!');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === prefix + 'Xena') {
+    msg.channel.sendMessage('Do not touch my master!');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === prefix + 'Pixel') {
+    msg.channel.sendMessage('Pixelate that. - Pixel, 2k17');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === prefix + 'ArX') {
+    msg.channel.sendMessage('Feel the power of the almighty ban hammer!');
+  }
+});
+
+client.login(process.env.TOKEN);
