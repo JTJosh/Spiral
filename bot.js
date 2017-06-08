eed76d5aa863a3b4d4cb28b9c81f5334ab75bb3f
JTJosh <joshuadominated@gmail.com>
2017-06-08 13:19:22 +1000

    Re-added >s command

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on('message', message => {
     let channel = message.channel;
 
 if (message.content === prefix + 'Ban')
-  message.reply('User banned succesfully!');
+  message.reply('User banned succesfully!')
 
 else if (message.content === prefix + 'XenaCorrupt')
   message.channel.send('You may fall from the sky, you may fall from a tree. But the best way to fall, is in love with me. <3');
@@ -60,8 +60,11 @@ else if (message.content.startsWith(prefix + 'Warm'))
 else if (message.content.startsWith(prefix + 'Abooses'))
   message.channel.send('*Abooses*');
 
+else if (message.content.startsWith(prefix + 'Fail'))
+  message.channel.send('*You Failed!*')
+
 else if (message.content.startsWith(prefix + 'welcome'))
- if(message.member.hasPermission("ADMINISTRATOR"))
+ if(message.member.hasPermission("MANAGE_MESSAGES"))
   message.channel.send(`${message.guild.roles.get('305302877641900052')} Welcome to Sinbadx Knights! **If you would like to get verified and be able to speak in the other channels, please answer the following questions!**\n1. How did you hear about this server?\n2. Why did you join this server?\n3. Do you promise to read <#297263352252727296>?\n4. What is your favorite diep.io tank?\nMake sure you send your answers in this channel. (Do not message them)`);
   else message.reply('You do not have the permission: ADMINISTRATOR');
 
@@ -72,17 +75,17 @@ else if (message.content.startsWith(prefix + 'help'))
 else if (message.content.startsWith(prefix + 'Spreadsheet'))
   message.channel.send ('The spreadsheet can be found at this site: https://docs.google.com/spreadsheets/d/18HlGT-Ys2Z5mFTD18QZeFgnVQunf1LqT5VxnddDnbuw/edit?usp=sharing ');
 
-else if (message.content.startsWith(prefix +'status'))
+else if (message.content.startsWith(prefix + 'Status'))
   try{
     let bad = new Discord.RichEmbed();
     let myavatar = `https://images-ext-1.discordapp.net/.eJwFwW0OwiAMANC7cABavgZdYjxLLUyX6EYA92PGu--9n_q2t5rVa4zaZwDJm85rl71lrlXL_gE-eHDr4Eya0BGhjc4FStFDoMUbEhaPjyRIJTKXYHMwU0Qk0nV73vt6lptFn9T_AohLIRY.DAP1B8BhF5M5YeVpwoK2kW_ov0c`;
     bad.setColor(message.member&&message.member.displayColor?message.member.displayColor:1290103);
     bad.setAuthor(client.user.username, client.user.avatarURL);
+    bad.addField('Total Users: ',`\`${client.users.size}\``);
     bad.setTitle('Bot Status').setDescription('Working');
     bad.addField('System Status: ','**__All systems functioning properly__**')
     bad.addField('CPU Usage:', `${process.cpuUsage().user}`)
        .addField('Packet Time: ',`\`${Date.now() - message.createdTimestamp} ms\``)
-       .addField('Uptime: ',`\`${process.uptime()} seconds\``)
        .addField('Avatar URL: ',`[click](myavatar)`)
        .addField('Timestamp: ',new Date().toUTCString());
     bad.setThumbnail(myavatar);
@@ -91,14 +94,15 @@ else if (message.content.startsWith(prefix +'status'))
     return message.channel.send(' ',{embed: bad});
   }catch(err){
     console.error('[CMD][Status]: '+err);
+}
 
 else if(message.content.startsWith(prefix + 's')){
-  if(message.author.id == JOSH || msg.author.id == WILLY){
-    channel.send(msg.content.split(' ').slice(1).join(' '));
+  if(message.author.id == JOSH || message.author.id == WILLY){
+    channel.send(message.content.split(' ').slice(1).join(' '));
     message.delete();
-   }
   }
- }
+}
+
 
 });
 
